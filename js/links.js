8a0e78700a3b55141c9a47e4d6fd740b4e1ac8d5
bubatz <71530056+cynhook@users.noreply.github.com>
2022-04-28 21:43:18 -0400

    Create links.js

---
+++ js/links.js
@@ -0,0 +1,8 @@
+var linkList = [
+    ["#1323b6", "#081054", "https://e-z.host", "e-z.host", 0],
+    ["#2a11cf", "#11084d", "https://discord.com/invite/ez", "discord", 0],
+    ["#4912c9", "#1a0647", "https://www.youtube.com/channel/UCo3FkSj5UcAOF25kqFsV0Zw", "youtube", 0],
+    ["#7111d1", "#2b064f", "https://www.instagram.com/aidenhasthememes", "instagram", 0],
+    ["#9a12cc", "#320742", "https://namemc.com/profile/SKRRRTT.7", "namemc", 0],
+    ["#c414c4", "#3d073d", "https://solo.to/skrrrtt", "solo.to", 0],
+];
