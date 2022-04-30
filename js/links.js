c946328c39b09256f3c7de4a4566c8dce8a9500d
amongusloverss <86531174+amongusloverss@users.noreply.github.com>
2022-04-29 23:32:34 -0400

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,8 +1,8 @@
 var linkList = [
-    ["#1323b6", "#081054", "https://e-z.host", "e-z.host", 0],
-    ["#2a11cf", "#11084d", "https://discord.com/invite/ez", "discord", 0],
-    ["#4912c9", "#1a0647", "https://www.youtube.com/channel/UCo3FkSj5UcAOF25kqFsV0Zw", "youtube", 0],
-    ["#7111d1", "#2b064f", "https://www.instagram.com/aidenhasthememes", "instagram", 0],
-    ["#9a12cc", "#320742", "https://namemc.com/profile/SKRRRTT.7", "namemc", 0],
-    ["#c414c4", "#3d073d", "https://solo.to/skrrrtt", "solo.to", 0],
+       ["#1323b6", "#081054", "https://twitter.com/SickSogga", "twitter", 0],
+    ["#2a11cf", "#11084d", "https://discord.com/users/960653177114460270", "discord", 0],
+    ["#4912c9", "#1a0647", "mailto:gb@crysis.email", "email", 0],
+    ["#7111d1", "#2b064f", "https://www.instagram.com/fridajasminn", "instagram", 0],
+    ["#9a12cc", "#320742", "https://namemc.com/profile/ghostbush.2", "namemc", 0],
+    ["#c414c4", "#3d073d", "https://www.last.fm/user/ghostbush", "last.fm", 0],
 ];
